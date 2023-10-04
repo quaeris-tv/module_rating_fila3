48ceb9cb778a007bcfada2a96a3e9f5fceda8b41
marco xot <marco.sottana@gmail.com>
2023-10-04 14:09:07 +0200

    up

--- Models/Rating.php
+++ Models/Rating.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\Rating\Models;
 
+use Modules\Rating\Enums\RuleEnum;
 use Illuminate\Database\Eloquent\Builder;
 use Spatie\SchemalessAttributes\Casts\SchemalessAttributes;
 
@@ -15,10 +16,17 @@ class Rating extends BaseModel
         'title',
         'color',
         'txt',
+        'rule',
+        'is_disabled',
+        'is_readonly',
+        'order_column',
     ];
 
     public $casts = [
         'extra_attributes' => SchemalessAttributes::class,
+        'rule'=>RuleEnum::class,
+        'is_disabled' => 'boolean',
+        'is_readonly'=> 'boolean',
     ];
 
     public function scopeWithExtraAttributes(): Builder
