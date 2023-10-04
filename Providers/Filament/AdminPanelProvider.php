8f6a771e1a8fe7dcb275a1ed0a71458b7cd757d3
marco xot <marco.sottana@gmail.com>
2023-10-04 10:58:42 +0200

    up

--- Providers/Filament/AdminPanelProvider.php
+++ Providers/Filament/AdminPanelProvider.php
@@ -1,20 +1,6 @@
 <?php
 
-<<<<<<< HEAD
 declare(strict_types=1);
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-declare(strict_types=1);
-
-=======
->>>>>>> c51c185 (.)
-=======
-declare(strict_types=1);
-
->>>>>>> 5fff76e (Check & fix styling)
-namespace App\Providers\Filament;
->>>>>>> 76d9daec57697b9fb9d80510854034aa2efae86b
 
 namespace Modules\Rating\Providers\Filament;
 
