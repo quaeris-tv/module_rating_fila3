7fc8b0418aa165cdffff6f06f739506116d4186d
marco xot <marco.sottana@gmail.com>
2023-12-20 13:15:47 +0100

    first

---
+++ Filament/Resources/RatingResource.php
@@ -0,0 +1,90 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Rating\Filament\Resources;
+
+use Filament\Forms\Components\ColorPicker;
+use Filament\Forms\Components\Radio;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\Section;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Form;
+use Filament\Tables;
+use Filament\Tables\Columns\IconColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ToggleColumn;
+use Filament\Tables\Table;
+use Modules\Rating\Enums\RuleEnum;
+use Modules\Rating\Filament\Resources\RatingResource\Pages;
+use Modules\Rating\Models\Rating;
+use Modules\Xot\Filament\Resources\XotBaseResource;
+
+class RatingResource extends XotBaseResource
+{
+    protected static ?string $model = Rating::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('extra_attributes.type'),
+                TextInput::make('extra_attributes.anno'),
+                TextInput::make('title')->autofocus()->required(),
+                ColorPicker::make('color'),
+                // TextInput::make('icon')->autofocus()->required(),
+                // Select::make('rule')->options(RuleEnum::class) ,
+                Radio::make('rule')->options(RuleEnum::class),
+                Section::make()
+                ->schema([
+                    Toggle::make('is_disabled'),
+                    Toggle::make('is_readonly'),
+                ])->columns(3),
+                RichEditor::make('txt')->columnSpanFull(),
+            ])->columns(3);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('extra_attributes.type')->label('type'),
+                TextColumn::make('extra_attributes.anno')->label('anno'),
+                TextColumn::make('title'),
+                TextColumn::make('rule')->badge(),
+                // TextColumn::make('is_readonly'),
+                // TextColumn::make('is_disabled'),
+                // ToggleColumn::make('is_readonly'),
+                IconColumn::make('is_disabled')->boolean(),
+                IconColumn::make('is_readonly')->boolean(),
+                // TextColumn::make('color'),
+            ])
+            ->filters([
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListRatings::route('/'),
+            'create' => Pages\CreateRating::route('/create'),
+            'edit' => Pages\EditRating::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ Models/BaseModel.php
@@ -0,0 +1,66 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Rating\Models;
+
+use Illuminate\Database\Eloquent\Model;
+// //use Laravel\Scout\Searchable;
+// ---------- traits
+use Modules\Xot\Traits\Updater;
+
+/**
+ * Class BaseModel.
+ */
+abstract class BaseModel extends Model
+{
+    use Updater;
+
+    /**
+     * Indicates whether attributes are snake cased on arrays.
+     *
+     * @see  https://laravel-news.com/6-eloquent-secrets
+     *
+     * @var bool
+     */
+    public static $snakeAttributes = true;
+
+    protected $perPage = 30;
+
+    // use Searchable;
+
+    protected $connection = 'rating'; // this will use the specified database connection
+    /**
+     * @var string[]
+     */
+    protected $fillable = ['id'];
+    /**
+     * @var array<string, string>
+     */
+    protected $casts = [
+        // 'published_at' => 'datetime:Y-m-d', // da verificare
+    ];
+
+    /**
+     * @var string[]
+     */
+    protected $dates = ['published_at', 'created_at', 'updated_at'];
+    /**
+     * @var string
+     */
+    protected $primaryKey = 'id';
+    /**
+     * @var bool
+     */
+    public $incrementing = true;
+    /**
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        // 'password'
+    ];
+    /**
+     * @var bool
+     */
+    public $timestamps = true;
+}
