28e43018bdea64ae00844e036f3b0b9895d004ab
marco76tv <marco.sottana@gmail.com>
2024-02-15 12:07:27 +0100

    up

--- Models/Rating.php
+++ Models/Rating.php
@@ -14,12 +14,42 @@ use Spatie\SchemalessAttributes\Casts\SchemalessAttributes;
  *
  * @property \Spatie\SchemalessAttributes\SchemalessAttributes $extra_attributes
  * @property RuleEnum                                          $rule
- *
  * @method static Builder|Rating newModelQuery()
  * @method static Builder|Rating newQuery()
  * @method static Builder|Rating query()
  * @method static Builder|Rating withExtraAttributes()
- *
+ * @property int $id
+ * @property string|null $related_type
+ * @property string|null $created_by
+ * @property string|null $updated_by
+ * @property string|null $deleted_by
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property int|null $post_id
+ * @property string|null $title
+ * @property string|null $color
+ * @property string|null $icon
+ * @property string|null $txt
+ * @property bool|null $is_disabled
+ * @property bool|null $is_readonly
+ * @property int|null $order_column
+ * @property-read \Illuminate\Database\Eloquent\Model|\Eloquent $linkedTo
+ * @method static Builder|Rating whereColor($value)
+ * @method static Builder|Rating whereCreatedAt($value)
+ * @method static Builder|Rating whereCreatedBy($value)
+ * @method static Builder|Rating whereDeletedBy($value)
+ * @method static Builder|Rating whereIcon($value)
+ * @method static Builder|Rating whereId($value)
+ * @method static Builder|Rating whereIsDisabled($value)
+ * @method static Builder|Rating whereIsReadonly($value)
+ * @method static Builder|Rating whereOrderColumn($value)
+ * @method static Builder|Rating wherePostId($value)
+ * @method static Builder|Rating whereRelatedType($value)
+ * @method static Builder|Rating whereRule($value)
+ * @method static Builder|Rating whereTitle($value)
+ * @method static Builder|Rating whereTxt($value)
+ * @method static Builder|Rating whereUpdatedAt($value)
+ * @method static Builder|Rating whereUpdatedBy($value)
  * @mixin \Eloquent
  */
 class Rating extends BaseModel
