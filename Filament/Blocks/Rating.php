5fca1cf6f54e9d50c1526334a3f557e08475e5a3
marco76tv <marco76tv@users.noreply.github.com>
2024-02-16 11:37:21 +0000

    Check & fix styling

--- Filament/Blocks/Rating.php
+++ Filament/Blocks/Rating.php
@@ -24,7 +24,7 @@ class Rating
                     TextInput::make('title')->required(),
                     ColorPicker::make('color'),
                     SpatieMediaLibraryFileUpload::make('avatar'),
-                        //->collection('avatars')
+                    // ->collection('avatars')
                 ])
                 ->reorderableWithButtons()
                 ->reorderableWithDragAndDrop(true)
