e19e1cda14dff4b8f1fabc44db4e1513ec27486f
marco76tv <marco76tv@users.noreply.github.com>
2024-05-02 08:11:59 +0000

    Check & fix styling

--- Actions/HasRating/GetSumByModelRatingIdAction.php
+++ Actions/HasRating/GetSumByModelRatingIdAction.php
@@ -1,27 +1,28 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Modules\Rating\Actions\HasRating;
 
-use Spatie\QueueableAction\QueueableAction;
 use Modules\Rating\Models\Contracts\HasRatingContract;
+use Spatie\QueueableAction\QueueableAction;
 
-
-
-class GetSumByModelRatingIdAction{
+class GetSumByModelRatingIdAction
+{
     use QueueableAction;
 
     /**
      * Undocumented function.
      */
-    public function execute(HasRatingContract $model,?string $rating_id=null): float
+    public function execute(HasRatingContract $model, ?string $rating_id = null): float
     {
-        $opts=$model->ratings()
-        ->wherePivot('user_id','!=',null);
-        if($rating_id!==null){
-            $opts=$opts->wherePivot('rating_id',$rating_id);
+        $opts = $model->ratings()
+        ->wherePivot('user_id', '!=', null);
+        if (null !== $rating_id) {
+            $opts = $opts->wherePivot('rating_id', $rating_id);
         }
-        $opts=$opts->sum('rating_morph.value');
-        
+        $opts = $opts->sum('rating_morph.value');
+
         return $opts;
     }
 }
--- Models/Contracts/HasRatingContract.php
+++ Models/Contracts/HasRatingContract.php
@@ -5,16 +5,15 @@ declare(strict_types=1);
 namespace Modules\Rating\Models\Contracts;
 
 /**
- * --
+ * --.
  */
 interface HasRatingContract
 {
-   
 }
 
 /*
  * @property-read string $url
- 
+
 interface Page
 {
     public function getUrlAttribute(): string;
