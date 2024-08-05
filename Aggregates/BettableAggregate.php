d3dee7bb6a2af423e621bd48e47bf529fa8906ac
Malebestia <vair81@gmail.com>
2024-08-05 16:24:18 +0200

    .

---
+++ Aggregates/BettableAggregate.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * @see https://github.com/cnastasi/event-sourcing-with-laravel/blob/main/app/Aggregates/ProductAggregate.php
+ */
+
+namespace Modules\Rating\Aggregates;
+
+use Spatie\EventSourcing\AggregateRoots\AggregateRoot;
+
+class BettableAggregate extends AggregateRoot
+{
+}
---
+++ Database/Migrations/2023_01_01_000005_create_rating_morph_table.php
@@ -0,0 +1,70 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Schema\Blueprint;
+// ----- models -----
+
+use Modules\Rating\Models\Rating;
+use Modules\User\Models\User;
+use Modules\Xot\Database\Migrations\XotBaseMigration;
+
+/**
+ * Class CreateRatingMorphTable.
+ */
+class CreateRatingMorphTable extends XotBaseMigration
+{
+    /**
+     * db up.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // ----- create -----
+        // -- CREATE --
+        $this->tableCreate(
+            static function (Blueprint $table) {
+                $table->id();
+                $table->foreignIdFor(Rating::class, 'rating_id')->nullable();
+                $table->nullableMorphs('model');
+                $table->foreignIdFor(User::class, 'user_id')->nullable();
+                $table->decimal('value', 10, 3)->nullable();
+                $table->text('note')->nullable();
+            }
+        );
+
+        // -- UPDATE --
+        $this->tableUpdate(
+            function (Blueprint $table) {
+                if (! $this->hasColumn('model_id')) {
+                    $table->nullableMorphs('model');
+                }
+
+                if (! $this->hasColumn('rating_id')) {
+                    $table->foreignIdFor(Rating::class, 'rating_id')->nullable();
+                }
+
+                if (! $this->hasColumn('note')) {
+                    $table->text('note')->nullable();
+                }
+
+                if (! $this->hasColumn('is_winner')) {
+                    $table->boolean('is_winner')->default(0);
+                }
+
+                if (! $this->hasColumn('reward')) {
+                    $table->decimal('reward', 10, 3)->default(0);
+                }
+
+                if ($this->hasColumn('value')) {
+                    $table->decimal('value', 10, 3)->nullable()->change();
+                } else {
+                    $table->decimal('value', 10, 3)->nullable();
+                }
+
+                $this->updateTimestamps(table: $table, hasSoftDeletes: true);
+            }
+        );
+    }
+}
