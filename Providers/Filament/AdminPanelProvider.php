ec9137b36b48c99deb5f92d0380ad23d8d942562
Malebestia <vair81@gmail.com>
2024-08-09 18:09:57 +0200

    rebase 15/37

---
+++ Providers/Filament/AdminPanelProvider.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * ---.
+ */
+
+declare(strict_types=1);
+
+namespace Modules\Rating\Providers\Filament;
+
+use Modules\Xot\Providers\Filament\XotBasePanelProvider;
+
+class AdminPanelProvider extends XotBasePanelProvider
+{
+    protected string $module = 'Rating';
+}
