8f9ecdd1e8c4a470acdefe48d60ce3e055190cc7
Malebestia <vair81@gmail.com>
2024-08-09 18:19:07 +0200

    rebase 26/34

---
+++ Database/Migrations/2023_01_01_000000_create_ratings_table.php
@@ -0,0 +1,62 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Schema\Blueprint;
+// ----- models -----
+use Modules\Xot\Database\Migrations\XotBaseMigration;
+
+/**
+ * Class CreateRatingsTable.
+ */
+class CreateRatingsTable extends XotBaseMigration
+{
+    /**
+     * db up.
+     */
+    public function up(): void
+    {
+        // -- CREATE --
+        $this->tableCreate(
+            static function (Blueprint $table) {
+                $table->id();
+                $table->string('title')->nullable();
+                $table->string('color')->nullable();
+                $table->string('icon')->nullable();
+                $table->string('rule')->nullable();
+                $table->text('txt')->nullable();
+            }
+        );
+
+        // -- UPDATE --
+        $this->tableUpdate(
+            function (Blueprint $table) {
+                if (! $this->hasColumn('title')) {
+                    $table->string('title')->nullable();
+                }
+                if (! $this->hasColumn('color')) {
+                    $table->string('color')->nullable();
+                }
+                if (! $this->hasColumn('icon')) {
+                    $table->string('icon')->nullable();
+                }
+                if (! $this->hasColumn('rule')) {
+                    $table->string('rule')->nullable();
+                }
+                if (! $this->hasColumn('txt')) {
+                    $table->string('txt')->nullable();
+                }
+                if (! $this->hasColumn('is_disabled')) {
+                    $table->boolean('is_disabled')->nullable();
+                }
+                if (! $this->hasColumn('is_readonly')) {
+                    $table->boolean('is_readonly')->nullable();
+                }
+                if (! $this->hasColumn('order_column')) {
+                    $table->unsignedInteger('order_column')->nullable()->index();
+                }
+                $this->updateTimestamps(table: $table, hasSoftDeletes: false);
+            }
+        );
+    }
+}
