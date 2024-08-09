e0fa30ec4bf833f773bd77c31f263b64793a839a
Malebestia <vair81@gmail.com>
2024-08-09 18:10:44 +0200

    rebase 25/37

--- Config/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    'name' => 'Rating',
-    'icon' => 'heroicon-o-star', // icon on dashboard
-    'navigation_sort' => 1,
-];
--- Resources/views/livewire/favorite/bs_italia.blade.php
+++
@@ -1,9 +0,0 @@
-<div>
-    <svg class="icon icon-primary" wire:click="update()">
-        @if ($fav)
-            <use xlink:href="{{ Theme::asset('pub_theme::dist/svg/sprite.svg') }}#it-star-full">
-            @else
-                <use xlink:href="{{ Theme::asset('pub_theme::dist/svg/sprite.svg') }}#it-star-outline">
-        @endif
-    </svg>
-</div>
