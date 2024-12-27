1e3c5678b4e136d15431d200f0935d410cbe36ac
Malebestia <vair81@gmail.com>
2024-12-27 16:44:13 +0100

    Here is a clean and descriptive commit message:
    "Initial commit of the Rating module for Xot. This commit includes the necessary files and directories for the module, including the service provider, route provider, admin panel provider, and view components. The module is designed to provide rating functionality for Xot."

---
+++ app/Providers/RatingServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * ---.
+ */
+
+declare(strict_types=1);
+
+namespace Modules\Rating\Providers;
+
+use Modules\Xot\Providers\XotBaseServiceProvider;
+
+class RatingServiceProvider extends XotBaseServiceProvider
+{
+    public string $module_name = 'Rating';
+    protected string $module_dir = __DIR__;
+    protected string $module_ns = __NAMESPACE__;
+}
